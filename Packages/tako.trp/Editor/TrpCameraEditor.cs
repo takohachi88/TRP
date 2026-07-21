57fad05d19d3aaafe673a6bdc9460477901fa15f
tako <tako>
2026-07-22 07:06:41 +0900

    カメラにlens shiftとgate fit追加

--- Packages/tako.trp/Editor/TrpCameraEditor.cs
+++ Packages/tako.trp/Editor/TrpCameraEditor.cs
@@ -23,6 +23,9 @@ namespace TrpEditor
 			public bool IsOrthographic;
 			public float? OrthographicSize;
 			public float? FieldOfView;
+			public bool UsePhysicalProperties;
+			public Vector2? LensShift;
+			public Camera.GateFitMode? GateFit;
 			public float Near;
 			public float Far;
 			public Rect Rect;
@@ -34,8 +37,11 @@ namespace TrpEditor
 				camera.clearFlags = ClearFlags;
 				if (BackgroundColor.HasValue) camera.backgroundColor = BackgroundColor.Value;
 				camera.orthographic = IsOrthographic;
+				camera.usePhysicalProperties = UsePhysicalProperties;
 				if (OrthographicSize.HasValue) camera.orthographicSize = OrthographicSize.Value;
 				if (FieldOfView.HasValue) camera.fieldOfView = FieldOfView.Value;
+				if (LensShift.HasValue) camera.lensShift = LensShift.Value;
+				if (GateFit.HasValue) camera.gateFit = GateFit.Value;
 				camera.nearClipPlane = Near;
 				camera.farClipPlane = Far;
 				camera.rect = Rect;
@@ -102,8 +108,31 @@ namespace TrpEditor
 					_properties.BackgroundColor = EditorGUILayout.ColorField("Background Color", _camera.backgroundColor);
 				}
 				_properties.IsOrthographic = EditorGUILayout.Toggle("Orthographic", _camera.orthographic);
-				if (_properties.IsOrthographic) _properties.OrthographicSize = Mathf.Max(0, EditorGUILayout.FloatField("Orthographic Size", _camera.orthographicSize));
-				else _properties.FieldOfView = EditorGUILayout.Slider("FoV", _camera.fieldOfView, 1, 179);
+				_properties.UsePhysicalProperties = _camera.usePhysicalProperties;
+				if (_properties.IsOrthographic)
+				{
+					_properties.OrthographicSize = Mathf.Max(0, EditorGUILayout.FloatField("Orthographic Size", _camera.orthographicSize));
+					_properties.LensShift = null;
+					_properties.GateFit = null;
+				}
+				else
+				{
+					_properties.FieldOfView = EditorGUILayout.Slider("FoV", _camera.fieldOfView, 1, 179);
+					// Unity標準と同じく、Lens Shiftが投影行列へ反映されるPhysical Cameraを明示的に切り替える。
+					_properties.UsePhysicalProperties = EditorGUILayout.Toggle("Physical Camera", _camera.usePhysicalProperties);
+					if (_properties.UsePhysicalProperties)
+					{
+						// 非対称透視投影により、カメラの向きを変えずに消失点を移動する。
+						_properties.LensShift = EditorGUILayout.Vector2Field("Lens Shift", _camera.lensShift);
+						// センサー領域と出力領域のアスペクト比が異なる場合の合わせ方を指定する。
+						_properties.GateFit = (Camera.GateFitMode)EditorGUILayout.EnumPopup("Gate Fit", _camera.gateFit);
+					}
+					else
+					{
+						_properties.LensShift = null;
+						_properties.GateFit = null;
+					}
+				}
 				_properties.Near = EditorGUILayout.FloatField("Near", _camera.nearClipPlane);
 				_properties.Far = EditorGUILayout.FloatField("Far", _camera.farClipPlane);
 				_properties.Rect = EditorGUILayout.RectField("View Port Rect", _camera.rect);
